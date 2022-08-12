d1400e577ec77edc3f07f3bf1780737e8e3f5994
Hien Le <leduyhien152@gmail.com>
2022-08-12 16:35:53 +0700

    fix: update new style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,7 @@
 import React, { useMemo } from 'react';
-import 'swiper/css';
-import 'swiper/css/bundle';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 import Providers from '../src/Providers';
 import Menu from '../src/views/menu';
 import './global.scss';
-import './swipe.css';
 
 function App({ Component, pageProps }) {
   const showMenu = useMemo(() => {
