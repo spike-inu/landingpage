afa1ccc2e644a2e8d019498a2b54a376d1ea53c9
Hien Le <leduyhien152@gmail.com>
2022-08-16 15:47:24 +0700

    fix: update new style

--- src/components/actions/LaunchApp.tsx
+++ src/components/actions/LaunchApp.tsx
@@ -3,7 +3,18 @@ import React, { FC, memo } from 'react';
 import { LoadingButtonProps } from '@mui/lab';
 
 const View: FC<LoadingButtonProps> = (props) => {
-  return <Pressable {...props}>Launch App</Pressable>;
+  return (
+    <Pressable
+      onClick={() => {
+        if (window) {
+          window.open('https://app.icrosschain.io/', '_blank');
+        }
+      }}
+      {...props}
+    >
+      Launch App
+    </Pressable>
+  );
 };
 
 export default memo(View);
