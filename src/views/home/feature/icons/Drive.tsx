47095c34537dd832cfbee8030c55f50ad935c268
Hien Le <leduyhien152@gmail.com>
2022-11-16 02:44:27 +0700

    fix: layout

--- src/views/home/feature/icons/Drive.tsx
+++ src/views/home/feature/icons/Drive.tsx
@@ -2,7 +2,7 @@ import { Image } from 'components';
 import React from 'react';
 
 const Icon = () => {
-  return <Image src="/images/feature-drive.png" width={50} loading="lazy" />;
+  return <Image src="/images/feature-drive.png" width={50} height={50} loading="lazy" />;
 };
 
 export default React.memo(Icon);
